40dd02ac78e96b47fb8e415bf8143617dee5f3c5
saurabh04metaphor <162103419+saurabh04metaphor@users.noreply.github.com>
2025-05-06 22:13:34 +0530

    Added a feature to view articles from all feeds at one place.

--- feed_reader/urls.py
+++ feed_reader/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='feed_list'),  # /feeds/
-    path('articles/<int:id>/', views.article_list, name='article_list'),  # /feeds/articles/1/
-    path('article/<int:pk>/', views.article_detail, name='article_detail'),  # /feeds/article/1/
-    path('article/<int:pk>/toggle_read/', views.toggle_read_status, name='toggle_read'),  # /feeds/article/1/toggle_read/
-    path('feed/<int:feed_id>/unsubscribe/', views.unsubscribe_feed, name='unsubscribe_feed'),  # /feeds/feed/1/unsubscribe/
+    path('articles/', views.article_list, name='all_articles'),  # New global articles view
+    path('articles/<int:id>/', views.article_list, name='article_list'),  # Feed-specific articles
+    path('article/<int:pk>/', views.article_detail, name='article_detail'),
+    path('article/<int:pk>/toggle_read/', views.toggle_read_status, name='toggle_read'),
+    path('feed/<int:feed_id>/unsubscribe/', views.unsubscribe_feed, name='unsubscribe_feed'),
 ]
